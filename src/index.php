f143db824cf1933e8b4785af3da40ed89f3a6f53
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2018-10-10 22:38:19 -0500

    Agrego funcion para enviar notificacion por FCM
    En la ruta POST para inserar las notificaciones estas son enviadas a
    travez de Firebase Cloud Messagin.
    Tambien agrego cabeceras para permitir el acceso a los metodos GET,
    POST, PUT, etc. asi como el acceso desde cualquier parte y permitir las
    peticiones HTTP

--- src/index.php
+++ src/index.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Content-Length, Accept-Encoding");
+header("Access-Control-Allow-Origin: *");
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
--- src/modelos/notificacion.model.php
+++ src/modelos/notificacion.model.php
@@ -4,11 +4,54 @@ require_once 'utils/constantes.php';
 
 class NotificacionModel {
 
+
+    private static function enviarNoti($noti){
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://fcm.googleapis.com/fcm/send",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_POSTFIELDS => '{
+                    "to" : "/topics/'.$noti['destinatario'].'",
+                    "data" : {
+                        "idNoti"  :  "'. $noti['id'] .'",
+                        "tituloNoti"  :  "'. $noti['titulo'] .'",
+                        "descripcionNoti" : "'. $noti['descripcion'] .'",
+                        "fechaNoti" : "'. $noti['fecha'] .'",
+                        "horaNoti" : "'. $noti['hora'] .'",
+                        "tipoNoti": "'.$noti['tipo'].'"
+                    },
+                    "priority": "high"
+                }',
+                CURLOPT_HTTPHEADER => array(
+                    "Authorization: key=AIzaSyDJNVzLTUhAVTS2p7CDAjuvMj2-l12P5ks",
+                    "Content-Type: application/json",
+                    "cache-control: no-cache"
+                ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     public static function insertar($notificacion) {
         $con = Conexion::getInstancia()->getConexion();
 
         $sentencia = $con->prepare(
-            'INSERT INTO '.TABLA_NOTIFICACIONES.' VALUES (null, :titulo, :descripcion, :tipo, :fecha, :hora);'
+            'INSERT INTO '.TABLA_NOTIFICACIONES.' VALUES (null, :titulo, :descripcion, :tipo, :fecha, :hora, :destinatario);'
         );
 
         $sentencia->bindParam('titulo', $notificacion['titulo']);
@@ -16,12 +59,26 @@ class NotificacionModel {
         $sentencia->bindParam('tipo', $notificacion['tipo']);
         $sentencia->bindParam('fecha', $notificacion['fecha']);
         $sentencia->bindParam('hora', $notificacion['hora']);
+        $sentencia->bindParam('destinatario', $notificacion['destinatario']);
 
         if( $sentencia->execute() ){
-            return [
-                'okay' => true,
-                'respuesta' =>  $con->lastInsertId()
-            ];
+            $id = $con->lastInsertId();
+
+            $notificacion['id'] = $id;
+
+            if( self::enviarNoti($notificacion) ){
+                return [
+                    'okay' => true,
+                    'respuesta' =>  $id
+                ];
+            }else{
+                return [
+                    'okay' => true,
+                    'respuesta' =>  $id,
+                    'error' => 'noti_no_enviada'
+                ];
+            }
+            
         }else{
             return [
                 'okay' => false,
