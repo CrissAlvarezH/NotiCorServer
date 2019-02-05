436dcfb3658ef1a82eb7c4fb39fd517e6e6892c2
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2019-02-05 18:38:57 -0500

    Move enviarNoti to BaseModel and change parameters

--- src/modelos/base.model.php
+++ src/modelos/base.model.php
@@ -88,6 +88,48 @@ class BaseModel {
         return false;
     }
 
+    public static function enviarNoti($accion, $cuerpoMsj, $topico){
+        $jsonFormateado = str_replace('"', "'", json_encode($cuerpoMsj));
+        
+        error_log('accion: '.$accion. ', cuerpo: '. $jsonFormateado.' topico: '.$topico);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://fcm.googleapis.com/fcm/send",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_POSTFIELDS => '{
+                    "to" : "/topics/'. $topico .'",
+                    "data" : {
+                        "accion"  :  "'. $accion .'",
+                        "cuerpo"  :  "'. $jsonFormateado .'"
+                    },
+                    "priority": "high"
+                }',
+                CURLOPT_HTTPHEADER => array(
+                    "Authorization: key=AIzaSyBgmTkX6hwtNoZ4aLSVfakdT3EQF7i_2Rc",
+                    "Content-Type: application/json",
+                    "cache-control: no-cache"
+                ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
 
 ?>
--- src/modelos/notificacion.model.php
+++ src/modelos/notificacion.model.php
@@ -1,51 +1,9 @@
 <?php 
 require_once 'datos/conexion.php';
+require_once 'modelos/base.model.php';
 require_once 'utils/constantes.php';
 
 class NotificacionModel {
-
-
-    private static function enviarNoti($noti){
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-                CURLOPT_URL => "https://fcm.googleapis.com/fcm/send",
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => "",
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 30,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "POST",
-                CURLOPT_POSTFIELDS => '{
-                    "to" : "/topics/'.$noti['destinatario'].'",
-                    "data" : {
-                        "idNoti"  :  "'. $noti['id'] .'",
-                        "tituloNoti"  :  "'. $noti['titulo'] .'",
-                        "descripcionNoti" : "'. $noti['descripcion'] .'",
-                        "fechaNoti" : "'. $noti['fecha'] .'",
-                        "horaNoti" : "'. $noti['hora'] .'",
-                        "tipoNoti": "'.$noti['tipo'].'"
-                    },
-                    "priority": "high"
-                }',
-                CURLOPT_HTTPHEADER => array(
-                    "Authorization: key=AIzaSyDJNVzLTUhAVTS2p7CDAjuvMj2-l12P5ks",
-                    "Content-Type: application/json",
-                    "cache-control: no-cache"
-                ),
-        ));
-
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-
-        curl_close($curl);
-
-        if ($err) {
-            return false;
-        } else {
-            return true;
-        }
-    }
 
     public static function insertar($notificacion) {
         $con = Conexion::getInstancia()->getConexion();
@@ -66,7 +24,7 @@ class NotificacionModel {
 
             $notificacion['id'] = $id;
 
-            if( self::enviarNoti($notificacion) ){
+            if( BaseModel::enviarNoti('notificacion', $notificacion, $notificacion['destinatario']) ){
                 return [
                     'okay' => true,
                     'respuesta' =>  $id
